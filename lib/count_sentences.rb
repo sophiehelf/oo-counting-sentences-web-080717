8fc0ee71cd787bac8bd8ed16c9c6a9aa84c66ca7
sophiehelf <sophie.helf@gmail.com>
2017-08-09 20:16:07 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,37 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+    	true
+    else
+    	false
+    end
   end
 
   def question?
-
+  	if self.end_with?("?")
+  		true
+  	else
+  		false
+  	end
   end
 
   def exclamation?
-
+  	if self.end_with?("!")
+  		true
+  	else
+  		false
+  	end
   end
 
   def count_sentences
-
+  	split_up = self.split(/[.!?]+/)
+  	if split_up.length > 0
+  		puts split_up.length
+  	else
+  		0
+  	end
+  	split_up.length
   end
+
 end
